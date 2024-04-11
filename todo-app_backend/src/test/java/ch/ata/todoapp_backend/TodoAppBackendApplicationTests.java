411cf55f92f82e7563cccc3aa10d3d93087d8997
atahanca <atahan.ca@gmail.com>
2024-04-11 10:06:01 +0200

    installed frontend and backend folders

---
+++ todo-app_backend/src/test/java/ch/ata/todoapp_backend/TodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ata.todoapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
