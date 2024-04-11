7336cfa61b429285e03e2aa953e2b1caaedec6fe
atahanca <atahan.ca@gmail.com>
2024-04-11 16:11:53 +0200

    created TaskService class

--- todo-app_backend/src/main/java/ch/ata/todoapp_backend/service/TaskService.java
+++ todo-app_backend/src/main/java/ch/ata/todoapp_backend/service/TaskService.java
@@ -1,5 +1,13 @@
 package ch.ata.todoapp_backend.service;
 
+import ch.ata.todoapp_backend.repository.TaskRepository;
+
 public class TaskService {
-    
+    private final TaskRepository taskRepository; // creates a variable of type TaskRepository. This means that taskRepository must be an instance of TaskRepository
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
 }
