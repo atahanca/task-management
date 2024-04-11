8b8e7eea16263e368d055a5b3c455e19dc8d0105
atahanca <atahan.ca@gmail.com>
2024-04-11 13:48:10 +0200

    created repository class

--- todo-app_backend/src/main/java/ch/ata/todoapp_backend/repository/TaskRepository.java
+++ todo-app_backend/src/main/java/ch/ata/todoapp_backend/repository/TaskRepository.java
@@ -1,10 +1,18 @@
 package ch.ata.todoapp_backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ch.ata.todoapp_backend.model.Task;
 
 public interface TaskRepository extends JpaRepository <Task, Integer> {
-    
-    
+    void deleteTaskById(Integer id);
+
+    Optional<Task> findTaskById(Integer id);
+    List<Task> findAll();
+    List<Task> findByName(String name);
+    List<Task> findByStatus(String status);
+    List<Task> findByDate(String date);
 } 
