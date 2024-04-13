09093d5b3496895cb6086eebfd34129af6908a0a
atahanca <atahan.ca@gmail.com>
2024-04-13 06:49:19 +0200

    CORS config setup

---
+++ todo-app_backend/src/main/java/ch/ata/todoapp_backend/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package ch.ata.todoapp_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // You can customize this to match your requirements
+                registry.addMapping("/**") // This allows all paths in your application.
+                    .allowedOrigins("http://localhost:3000") // This should match the URL of your React frontend.
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
